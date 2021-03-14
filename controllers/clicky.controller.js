5b4ff8453b0c77d7537ada4be2677d6728456af6
Lorna <hello@lorna.dev>
2021-03-14 00:58:10 +0000

    clicky routes

--- controllers/clicky.controller.js
+++ controllers/clicky.controller.js
@@ -15,3 +15,21 @@ exports.getDownloadsCount = (req, res) => {
       res.send(err);
     });
 };
+
+// CV Downloads:
+
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=actions-downloads&date=2019
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=actions-downloads&date=2020
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=actions-downloads&date=2021
+
+// Visitors:
+
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=visitors&date=2019
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=visitors&date=2020
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=visitors&date=2021
+
+// Unique Visitors:
+
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=visitors-unique&date=2019
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=visitors-unique&date=2020
+// https://api.clicky.com/api/stats/4?site_id=${process.env.CLICKY_SITE_ID}&sitekey=${process.env.CLICKY_SITE_KEY}&type=visitors-unique&date=2021
