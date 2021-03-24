5bd97ee82a2afe936da31810ae549755ccf0137a
Lorna <hello@lorna.dev>
2021-03-24 00:12:31 +0000

    filter repo request

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -6,7 +6,18 @@ exports.getAllPosts = (req, res) => {
   axios
     .get(`https://dev.to/api/articles?username=lornasw93`)
     .then((resp) => {
-      res.send(resp.data);
+      if (resp.data.length > 0) {
+        var r = resp.data.sort((a, b) => {
+          return a.public_reactions_count - b.public_reactions_count
+        }).reverse();
+
+        var posts = r.slice(0, 8).sort((a, b) => {
+          var dateA = new Date(a.created_at), dateB = new Date(b.created_at);
+          return dateA.getDate() - dateB.getDate();
+        });
+
+        res.send(posts);
+      }
     })
     .catch((err) => {
       res.send(err);
